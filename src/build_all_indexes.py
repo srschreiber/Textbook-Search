6abe4d6ed0e3377869addf69f6f13d83c6f8cfae
Sam Schreiber <srschreiber@github.com>
2024-11-13 18:01:18 -0800

    add harmonic mean

--- src/build_all_indexes.py
+++ src/build_all_indexes.py
@@ -4,6 +4,18 @@ from config import Config
 from lib.tokenizer import Tokenizer
 from lib.my_faiss_index import FaissIndex
 
+def harmonic_mean_with_beta(p, r, beta=1):
+    # Higher beta gives more weight to r, while lower beta gives more weight to p
+    beta = max(beta, 0.0)
+    beta = min(beta, 1.0)
+
+    if p + r == 0:  
+        return 0.0
+
+    beta_squared = beta ** 2
+    f_beta_score = (1 + beta_squared) * (p * r) / (beta_squared * p + r)
+    return f_beta_score
+
 if __name__ == "__main__":
     config = Config()
     tokenizer = Tokenizer(config)
@@ -26,9 +38,15 @@ if __name__ == "__main__":
     # now rerank the results from bm25 with faiss
     faiss_reranked = faiss_index.rank_doc_ids(query, doc_ids_to_rerank)
 
+    reranked_doc_ids = [doc_id for doc_id, _ in faiss_reranked]
+
     for doc_id, score in faiss_reranked[:5]:
         sentence = original_sentences[doc_id]
         print(f"HIT {doc_id}: {sentence}\n\n")
+
+
+    # weighted harmonic mean between bm25 and faiss scores
+
 
     
 
